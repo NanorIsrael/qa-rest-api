9135c19d0ac2288ea6ca1b37c6692b3a3528c0d9
nanorisrael <israel.nanor@compozelabs.com>
2024-07-23 23:00:48 +0000

    Add route definitions

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict'
 import express from 'express';
+
+import router from './routes';
+
 const app = express();
 
 app.use(express.json())
---
+++ routes.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+	
+})
+
+router.get('/:qID', function (req, res) {
+	
+});
+
+router.get('/:qID/answers', function (req, res) {
+	
+});
+
+router.get('/:qID/answers/:aID', function (req, res) {
+	
+});
+
+router.get('/:qID/answers/:aID/vote-:dir', function (req, res) {
+	
+});
+
+router.post('/', function (req, res) {
+
+})
+export default router;
